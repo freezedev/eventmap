d357af3a07193da466b08b3fc8dfc3b5e7cdb9e0
Johannes Stein <johannesstein@freeze-dev.com>
2015-11-08 01:35:17 +0100

    Add PhantomJS ES5 shims

--- karma.conf.js
+++ karma.conf.js
@@ -3,7 +3,7 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['mocha', 'chai'],
+    frameworks: ['phantomjs-shim', 'mocha', 'chai'],
     files: [
       'dist/eventmap.js',
       'node_modules/chance/chance.js',
